3b7b52f556062a25f186475a0dae51725af72993
김지수 <118232697+tttghost@users.noreply.github.com>
2025-08-27 20:11:53 +0900

    Create HelloTest.cs

---
+++ HelloTest.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace TestUPM {
+    public class HelloTest : MonoBehaviour {
+        void Start() {
+            Debug.Log("Hello from TestUPM package!");
+        }
+    }
+}
